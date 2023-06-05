296779653fc1ca169735dfaf12becbf44b2d7933
Abdulbasit <basttyy@gmail.com>
2023-06-05 20:00:13 +0100

    added queue worker

--- src/Console/BurriedJobRunner.php
+++ src/Console/BurriedJobRunner.php
@@ -1,109 +1,50 @@
 <?php
-require_once './vendor/autoload.php';
-require_once 'src/libs/helpers.php';
+require_once __DIR__."/../../vendor/autoload.php";
+require_once __DIR__."/../libs/helpers.php";
 
 use Basttyy\FxDataServer\Console\QueueInterface;
 use Basttyy\FxDataServer\Console\Job_Queue;
+use Dotenv\Dotenv;
 
-$job_Queue = new Job_Queue('mysql', [
-    'mysql' => [
+$dotenv = Dotenv::createImmutable(__DIR__."/../../");
+$dotenv->safeLoad();
+
+$dotenv->required(['DB_ADAPTER', 'DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS', 'DB_PORT', 'DB_CHARSET', 'CHRON_INTERVAL'])->notEmpty();
+
+$dbtype = env('DB_ADAPTER');
+$dbhost = env("DB_HOST");
+$dbname = env('DB_NAME');
+$dbuser = env('DB_USER');
+$dbpass = env('DB_PASS');
+$dbport = env('DB_PORT');
+$dbcharset = env('DB_CHARSET');
+$chron_interval = env('CHRON_INTERVAL');
+
+$start_time = time();
+$end_time = time() + $chron_interval;
+
+$job_Queue = new Job_Queue(Job_Queue::QUEUE_TYPE_MYSQL, [
+    $dbtype => [
         'table_name' => 'jobs',     //the table that jobs will be stored in
         'use_compression' => true
     ]
 ]);
 
-$pdo = new PDO('mysql:dbname=tcpmtbridge;host=127.0.0.1', 'root', '123456789');
+$pdo = new PDO("$dbtype:dbname=$dbname;host=$dbhost", $dbuser, $dbpass);
 $job_Queue->addQueueConnection($pdo);
 $job_Queue->watchPipeline('default');
 
-///TODO: Job_Queue class db based methods should use 
-$job = await(\React\Promise\resolve($job_Queue->getNextJobAndReserve()));
+$job = $job_Queue->getNextBuriedJob();
 
-if(empty($job)) {
-    $sleeping1 = true;
-    $delay = await(sleep(1, $loop));
-    $sleeping1 = false;
-    return;
-}
-$jobs_count++;
-
-echo "Processing job {$job['id']}\n";
-$payload = $job['payload'];
-
-try {
-    $job_obj = unserialize($payload);
-
-    if ($job_obj instanceof QueueInterface) {
-        $job_obj->setJob($job);
-        $job_obj->setQueue($job_Queue);
-        $resp = await ($job_obj->handle());
-    }
-} catch(Exception $e) {
-    $job_Queue->buryJob($job, $job_obj->getDelay());
-}
-
-$sleeping1 = false;
-$sleeping2 = false;
-$jobs_count = 0;
-$bury_count = 0;
-$max_jobs = 10;
-$bury_max_jobs = 10;
-
-$loop->addPeriodicTimer(0.01, async(function (TimerInterface $timer) use ($loop, $job_Queue, &$jobs_count, $max_jobs, &$sleeping1) {
-    if ($sleeping1)
-        return;
-
-    if ($jobs_count >= $max_jobs)
-        return;
-    
-    ///TODO: Job_Queue class db based methods should use 
-    $job = await(\React\Promise\resolve($job_Queue->getNextJobAndReserve()));
-    
-    if(empty($job)) {
-        $sleeping1 = true;
-        $delay = await(sleep(1, $loop));
-        $sleeping1 = false;
-        return;
-    }
-    $jobs_count++;
-
-    echo "Processing job {$job['id']}\n";
-    $payload = $job['payload'];
-
-    try {
-        $job_obj = unserialize($payload);
-
-        if ($job_obj instanceof QueueInterface) {
-            $job_obj->setJob($job);
-            $job_obj->setQueue($job_Queue);
-            $resp = await ($job_obj->handle());
-        }
-    } catch(Exception $e) {
-        $job_Queue->buryJob($job, $job_obj->getDelay());
-    }
-    $jobs_count--;
-}));
-
-$loop->addPeriodicTimer(0.01, async(function ($timer) use ($loop, $bury_max_jobs, $job_Queue, &$bury_count, &$sleeping2) {
-    if ($sleeping2)
-        return;
-    
-    if ($bury_count >= $bury_max_jobs) {
-        return;
-    }
-    $job = await(\React\Promise\resolve($job_Queue->getNextBuriedJob()));
-
+while ($end_time > time()) {
     if (empty($job)) {
-        $sleeping2 = true;
-        $delay = await(sleep(1, $loop));
-        $sleeping2 = false;
-        return;
+        sleep(1);
+        echo "buried job is empty".PHP_EOL;
+        continue;
     }
-    $bury_count++;
 
     echo "Processing job {$job['id']}\n";
     $payload = $job['payload'];
-    $delay = await(sleep(4, $loop));
 
     try {
         $job_obj = unserialize($payload);
@@ -111,13 +52,9 @@ $loop->addPeriodicTimer(0.01, async(function ($timer) use ($loop, $bury_max_jobs
         if ($job_obj instanceof QueueInterface) {
             $job_obj->setJob($job);
             $job_obj->setQueue($job_Queue);
-            $resp = await($job_obj->handle());
+            $resp = $job_obj->handle();
         }
-        $bury_count--;
     } catch (Exception $e) {
-        $bury_count--;
         $job_Queue->buryJob($job, $job_obj->getDelay());
     }
-}));
-
-$loop->run();
+}
--- src/Console/JobRunner.php
+++ src/Console/JobRunner.php
@@ -1,60 +1,61 @@
 <?php
-require_once __DIR__.'/vendor/autoload.php';
-require_once __DIR__.'src/libs/helpers.php';
+require_once __DIR__."/../../vendor/autoload.php";
+require_once __DIR__."/../libs/helpers.php";
 
 use Basttyy\FxDataServer\Console\QueueInterface;
 use Basttyy\FxDataServer\Console\Job_Queue;
 use Dotenv\Dotenv;
 
-$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv::createImmutable(__DIR__."/../../");
 $dotenv->safeLoad();
 
-$dotenv->required(['DB_ADAPTER', 'DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PORT', 'DB_CHARSET', 'CHRON_INTERVAL'])->notEmpty();
+$dotenv->required(['DB_ADAPTER', 'DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS', 'DB_PORT', 'DB_CHARSET', 'CHRON_INTERVAL'])->notEmpty();
 
-$dbtype = getenv('DB_ADAPTER');
-$dbhost = getenv("DB_HOST");
-$dbname = getenv('DB_NAME');
-$dbuser = getenv('DB_USER');
-$dbport = getenv('DB_PORT');
-$dbcharset = getenv('DB_CHARSET');
-$chron_interval = getenv('CHRON_INTERVAL');
+$dbtype = env('DB_ADAPTER');
+$dbhost = env("DB_HOST");
+$dbname = env('DB_NAME');
+$dbuser = env('DB_USER');
+$dbpass = env('DB_PASS');
+$dbport = env('DB_PORT');
+$dbcharset = env('DB_CHARSET');
+$chron_interval = env('CHRON_INTERVAL');
 
 $start_time = time();
-$end_time = time() + env('CHRON_INTERVAL');
+$end_time = time() + $chron_interval;
+echo $dbtype.PHP_EOL;
 
-$job_Queue = new Job_Queue('mysql', [
-    'mysql' => [
+$job_Queue = new Job_Queue(Job_Queue::QUEUE_TYPE_MYSQL, [
+    $dbtype => [
         'table_name' => 'jobs',     //the table that jobs will be stored in
         'use_compression' => true
     ]
 ]);
 
-$pdo = new PDO("D:dbname=tcpmtbridge;host=127.0.0.1", 'root', '123456789');
+$pdo = new PDO("$dbtype:dbname=$dbname;host=$dbhost", $dbuser, $dbpass);
 $job_Queue->addQueueConnection($pdo);
 $job_Queue->watchPipeline('default');
 
-///TODO: Job_Queue class db based methods should use 
-$job = $job_Queue->getNextJobAndReserve();
-
-if(empty($job)) {
-    $sleeping1 = true;
-    $delay = await(sleep(1, $loop));
-    $sleeping1 = false;
-    return;
-}
-$jobs_count++;
-
-echo "Processing job {$job['id']}\n";
-$payload = $job['payload'];
-
-try {
-    $job_obj = unserialize($payload);
-
-    if ($job_obj instanceof QueueInterface) {
-        $job_obj->setJob($job);
-        $job_obj->setQueue($job_Queue);
-        $resp = $job_obj->handle();
+while ($end_time > time()) {
+    ///TODO: Job_Queue class db based methods should use 
+    $job = $job_Queue->getNextJobAndReserve();
+    
+    if(empty($job)) {
+        sleep(1);
+        echo "job is empty".PHP_EOL;
+        continue;
     }
-} catch(Exception $e) {
-    $job_Queue->buryJob($job, $job_obj->getDelay());
+
+    $payload = $job['payload'];
+
+    try {
+        $job_obj = unserialize($payload);
+
+        if ($job_obj instanceof QueueInterface) {
+            $job_obj->setJob($job);
+            $job_obj->setQueue($job_Queue);
+            $resp = $job_obj->handle();
+        }
+    } catch(Exception $e) {
+        $job_Queue->buryJob($job, $job_obj->getDelay());
+    }
 }
