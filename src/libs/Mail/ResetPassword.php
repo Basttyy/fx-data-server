03d55a4636bf650afc5f0dc6e9a23875f97b0679
Abdulbasit <basttyy@gmail.com>
2023-06-06 22:49:36 +0100

    added script for sending emails

--- src/libs/Mail/ResetPassword.php
+++ src/libs/Mail/ResetPassword.php
@@ -11,7 +11,7 @@ class VerifyEmail
 
     public function __construct(string $address, string $name, string $subject, string $html)
     {
-        self::$mail = new BaseMailer(true, env(['NOREPLY_EMAIL_USER']), env(['NOREPLY_EMAIL_PASSWORD']), $html);
+        self::$mail = new BaseMailer(true, env('NOREPLY_EMAIL_USER'), env('NOREPLY_EMAIL_PASSWORD'), $html);
 
         self::$mail->addAddress($address, $name);
         self::$mail->setFrom(env('NOREPLY_EMAIL_USER'), env('NOREPLY_EMAIL_NAME'));
@@ -31,7 +31,7 @@ class VerifyEmail
             'contents' => [
                 $name ? "Hello $name," : "Hy,",
                 "It seems you lost your password or u wish to change it?",
-                "If it wasn't you please disregard this email and make sure you can still login to your account. If it was you, then <span style='font-weight: 700;'>use this code $code to Reset Your Password or click the “Reset Now Button” below to Reset your password.</span>"
+                "If it wasn't you please disregard this email and make sure you can still login to your account. If it was you, then <span style='font-weight: 400;'>use this code <strong>$code</strong> to Reset Your Password or click the “Reset Now Button” below to Reset your password.</span>"
             ],
             'links' => [
                 'Verify Email' => "https://backtestfx.com/account/ressetpass?code=$code"
