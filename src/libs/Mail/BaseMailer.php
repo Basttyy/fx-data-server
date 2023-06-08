3ecd3c8ed93331213ff9c2ca1646a6f15d64a393
Abdulbasit <basttyy@gmail.com>
2023-06-08 21:51:20 +0100

    minor fix

--- src/libs/Mail/BaseMailer.php
+++ src/libs/Mail/BaseMailer.php
@@ -34,7 +34,7 @@ class BaseMailer extends PHPMailer
         //Set an HTML and plain-text body, import relative image references
         $this->msgHTML($body, './images/');
         //Show debug output
-        $this->SMTPDebug = SMTP::DEBUG_SERVER;
+        $this->SMTPDebug = env('APP_ENV') === 'local' ? SMTP::DEBUG_SERVER : SMTP::DEBUG_OFF;
 
         //Inject a new debug output handler
         $this->Debugoutput = static function ($str, $level) {
