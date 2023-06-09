c7a1b07e745e7204f7a75b1398ba287be00cdeff
Abdulbasit <basttyy@gmail.com>
2023-06-09 08:44:42 +0100

    debug fix

--- src/libs/Mail/VerifyEmail.php
+++ src/libs/Mail/VerifyEmail.php
@@ -25,7 +25,7 @@ class VerifyEmail
     public static function send(string $address, string $name, string $subject, string $code)
     {
         try {
-            $html = Templater::view('verify.html', '/mail/html/', [
+            $html = Templater::view('verify.html', 'mail/html/', [
                 'title' => "Email Verification",
                 'header' => "Email Verification",
                 'sender_email' => env('NOREPLY_EMAIL_USER'),  //$sender->email,
