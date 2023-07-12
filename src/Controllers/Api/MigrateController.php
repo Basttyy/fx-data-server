8f22fed4adda5eb6644e60a08fb7d972e2f59e3b
Abdulbasit <basttyy@gmail.com>
2023-07-12 17:09:45 +0100

    fix and secure migrations endpoint

--- src/Controllers/Api/MigrateController.php
+++ src/Controllers/Api/MigrateController.php
@@ -41,18 +41,18 @@ final class MigrateController
             // Get the environment and target version parameters.
             $env = sanitize_data($_GET['e']) ?? null;
             $target = sanitize_data($_GET['t']) ?? null;
-            $configuration = $_SERVER['DOCUMENT_ROOT']."/../phinx.php";
 
             // Check if debugging is enabled.
             $debug = !empty($_GET['debug']) && filter_var($_GET['debug'], FILTER_VALIDATE_BOOLEAN);
 
             // Execute the command and determine if it was successful.
-            $output = call_user_func([$this->wrap, $routes[$command]], $env, $target, $configuration);
+            $this->wrap->setOption('configuration', $_SERVER['DOCUMENT_ROOT']."/../phinx.php");
+            $output = call_user_func([$this->wrap, $routes[$command]], $env, $target);
             $error = $this->wrap->getExitCode() > 0;
 
             // Finally, display the output of the command.
             if ($error) {
-                $args = implode(', ', [var_export($env, true), var_export($target, true)], var_export($configuration, true));
+                $args = implode(', ', [var_export($env, true), var_export($target, true)]);
                 return JsonResponse::serverError("OUTPUT: $output --- DEBUG: $command($args)");
             } else {
                 $args = implode(', ', [var_export($env, true), var_export($target, true)]);
